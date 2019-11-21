60e29dc709677413be8a8d7731b5a930878687f0
jsolahr <51134556+jsolahr@users.noreply.github.com>
2019-11-20 18:44:55 -0600

    adding initial files

---
+++ clickygame/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import HelloBootstrap from "./components/HelloBootstrap";
+
+
+function App() {
+  return (
+    <HelloBootstrap></HelloBootstrap>
+  );
+}
+
+export default App;
