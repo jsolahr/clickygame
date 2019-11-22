650ebf2cce7d52a1639d7bcdc0205ac5299885dd
jsolahr <51134556+jsolahr@users.noreply.github.com>
2019-11-21 20:50:35 -0600

    adding initial files

--- src/components/HelloBootstrap.js
+++ src/components/HelloBootstrap.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function HelloBootstrap() {
   return (
+   
     <div className="container">
       <div className="jumbotron">
-        <h1>Hello, world!</h1>
-        <p>I'm a jumbotron!</p>
+        <h1>Clicky Game</h1>
         <p>
           <button className="btn btn-primary btn-lg">Learn more</button>
         </p>
@@ -15,6 +15,7 @@ function HelloBootstrap() {
           <h3>Card title</h3>
         </div>
         <div className="card-body">
+        <img src="https://1v1d1e1lmiki1lgcvx32p49h8fe-wpengine.netdna-ssl.com/wp-content/uploads/2019/09/1567916337-GettyImages-160987733-e1567916463401-960x540.jpg" alt="doggie"></img>
           <p className="card-text">Card content</p>
         </div>
       </div>
